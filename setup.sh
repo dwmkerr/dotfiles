22d2ca7bdfba9d4be16ea443fabc2c51f670fbbf
Dave Kerr <dwmkerr@gmail.com>
2016-08-06 14:50:19 +0800

    Added linking scripts

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@
 # Sets up a machine with my preferred config.
 
 # First, determine OS.
-OS = TODO
+#OS = TODO
 
 # Run the appropriate OS setup scripts.
 # if osx ./setup-osx.sh
 # if linux ./setup-linux.sh
 
 # Symlink the dotfiles.
-
+ln -s .vimrc ~/.vimrc
