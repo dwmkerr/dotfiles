a0e7fb754d07a08323c18e0d77b86254b88c102d
Dave Kerr <dwmkerr@gmail.com>
2017-11-21 17:26:32 +0700

    adds aliases

---
+++ aliases.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+# alises.sh
+#
+# Contains aliases for working with the shell.
+
+# ---
+#
+# General purposes aliases.
+#
+# ---
+
+# Remove formatting from text in the clipboard.
+alias clipclean="pbpaste | pbcopy"
+
+# ---
+#
+# OpenShift Aliases
+#
+# ---
+
+# I have to do this way too often
+alias killpod="oc delete pod --grace-period=0 "
