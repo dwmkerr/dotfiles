6256819f3b104e9e533f9a7b0ff78c36aac4b7ce
Dave Kerr <dwmkerr@gmail.com>
2017-12-04 16:07:18 +0700

    aliases: add the gbr alias to see more detail for local branches

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 # alises.sh
 #
@@ -21,3 +21,16 @@ alias clipclean="pbpaste | pbcopy"
 
 # I have to do this way too often
 alias killpod="oc delete pod --grace-period=0 "
+
+# ---
+#
+# Git Commands
+#
+# ---
+
+# Not really a command, but a much nicer version of git branch.
+# Source: https://stackoverflow.com/questions/2514172/listing-each-branch-and-its-lastevisions-date-in-git
+alias gbr='for k in `git branch -l | \
+    perl -pe '\''s/^..(.*?)( ->.*)?$/\1/'\''`; \
+    do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k -- | \
+    head -n 1`\\t$k; done | sort -r'
