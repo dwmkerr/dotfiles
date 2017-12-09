a28e495dad85fc6a70b5ce6a7f703885bcf0c7f5
Dave Kerr <dwmkerr@gmail.com>
2017-12-09 16:38:16 +0700

    profile: move git functions into their own file

---
+++ profile/git.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# ---
+#
+# Git Commands
+#
+# ---
+
+# Not really a command, but a much nicer version of git branch.
+# Source: https://stackoverflow.com/questions/2514172/listing-each-branch-and-its-lastevisions-date-in-git
+alias gbranch='for k in `git branch -l | \
+    perl -pe '\''s/^..(.*?)( ->.*)?$/\1/'\''`; \
+    do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k -- | \
+    head -n 1`\\t$k; done | sort -r'
