c9753069a632612ba674e22d4ee8436950ba8cc6
Dave Kerr <dwmkerr@gmail.com>
2017-12-10 10:59:47 +0700

    profile: source .private/* files automatically

--- profile.sh
+++ profile.sh
@@ -6,3 +6,12 @@ source ~/.environment.sh
 # Import everything from the .profile folder.
 source ~/.profile/aliases.sh
 source ~/.profile/openshift.sh
+
+# If we have a .private folder, source everything in it. This is useful for
+# automatically loading things like project specific secrets.
+if [[ -d ~/.private ]]; then
+    for private in ~/.private/*; do
+        [ -e "$private" ] || continue
+        source $private
+    done
+fi
