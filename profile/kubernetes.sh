1dc4f4de1c1e5f13f2da12e55c54c7568dba2cab
Dave Kerr <dwmkerr@gmail.com>
2018-07-05 08:45:36 +0800

    feat(profile): better kubernetes structure, kubernetes auto complete, GCP auto complete

--- profile/kubernetes.sh
+++ profile/kubernetes.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Kubernetes and OpenShift config.
+alias k='kubectl'
+
+
 # Log into to the locally configured OpenShift instance.
 function oslogin() {
   oc login $OPENSHIFT_URL -u $OPENSHIFT_USER -p $OPENSHIFT_PASS --insecure-skip-tls-verify
@@ -10,3 +14,6 @@ function podlogs() {
   echo "Getting logs for $1 for the last $2 duration"
   oc logs -f --since=$2 `oc get pods | grep $1 | grep 'Running' | grep -Ev 'deploy' | awk '{print $1}'`
 }
+
+# I have to do this way too often
+alias killpod="oc delete pod --grace-period=0 "
