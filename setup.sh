1e2e4c0b7ab732ce95f92b13ab65bf16a6fab1e2
Dave Kerr <dwmkerr@gmail.com>
2018-09-18 11:18:10 +0900

    chore: fix gpg key id

--- setup.sh
+++ setup.sh
@@ -110,7 +110,7 @@ fi
 echo "$os: Configuring Git for dwmkerr and GPG signing..."
 git config --global user.name "Dave Kerr"
 git config --global user.email "dwmkerr@gmail.com"
-git config --global user.signingKey "W35D965FB60ACC2E94E605038F780C45862199FEC"
+git config --global user.signingKey "35D965FB60ACC2E94E605038F780C45862199FEC"
 git config --global commit.gpgSign true
 git config --global tag.forceSignAnnotated true
 git config --global gpg.program "gpg2"
