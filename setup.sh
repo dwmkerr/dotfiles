eb6dc7c0c703cf4f5371860cb0b4470d8a8983d7
Dave Kerr <dwmkerr@gmail.com>
2019-07-02 15:04:23 +0800

    feat(mac): terraform auto complete

--- setup.sh
+++ setup.sh
@@ -233,6 +233,11 @@ if ask "$os: Setup Terraform and Terraform Lint?" Y; then
         brew install terraform
         brew tap wata727/tflint
         brew install tflint
+
+        # Now that terraform is installed, we can enable auto-completion.
+        # Note that this won't take effect until the shell is restarted.
+        terraform -install-autocomplete
+
     elif [[ "$os" == "ubuntu" ]]; then
         echo "$os: TODO"
     fi
