42a86dcf3ea7c5b6ef5ceee0835ecd53f177858a
Dave Kerr <dwmkerr@gmail.com>
2020-04-21 16:36:06 +0800

    refactor: split k8s and openshift config

--- profile.d/kubernetes.sh
+++ profile.d/kubernetes.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
 
-# Kubernetes and OpenShift config.
+# This has saved me a lot of time...
 alias k='kubectl'
-
-# Log into to the locally configured OpenShift instance.
-function oslogin() {
-  oc login $OPENSHIFT_URL -u $OPENSHIFT_USER -p $OPENSHIFT_PASS --insecure-skip-tls-verify
-  oc project $1
-}
-
-function podlogs() {
-  echo "Getting logs for $1 for the last $2 duration"
-  oc logs -f --since=$2 `oc get pods | grep $1 | grep 'Running' | grep -Ev 'deploy' | awk '{print $1}'`
-}
-
-# I have to do this way too often
-alias killpod="oc delete pod --grace-period=0 "
---
+++ profile.d/openshift.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# OpenShift config.
+
+# Log into to the locally configured OpenShift instance.
+function oslogin() {
+  oc login $OPENSHIFT_URL -u $OPENSHIFT_USER -p $OPENSHIFT_PASS --insecure-skip-tls-verify
+  oc project $1
+}
+
+function podlogs() {
+  echo "Getting logs for $1 for the last $2 duration"
+  oc logs -f --since=$2 `oc get pods | grep $1 | grep 'Running' | grep -Ev 'deploy' | awk '{print $1}'`
+}
+
+# I have to do this way too often
+alias killpod="oc delete pod --grace-period=0 "
