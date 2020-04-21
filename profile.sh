a655e23aa7422d0bda388c7c9da1b19f7ec71693
Dave Kerr <dwmkerr@gmail.com>
2020-04-21 16:58:48 +0800

    chore: cleanup bash ps1 and move k8s autocompletion setup to setup.d

--- profile.sh
+++ profile.sh
@@ -9,8 +9,6 @@
 # an example of why this matters. If we use a shebang in this script, we'll only
 # ever source auto-completions for the shell in the shebang.
 
-#!/usr/bin/env bash
-
 # Import everything from the .profile.d folder.
 for file in $HOME/.profile.d/*; do
     [ -e "$file" ] || continue
@@ -26,14 +24,6 @@ if [[ -d $HOME/.private ]]; then
     done
 fi
 
-# Bit of an issue with this - if we are in zsh we need zsh auto-completion, but
-# we're using a bash shebang...
-
-# If kubectl is installed, enable auto completion.
-# if [ -x "$(command -v kubectl)" ]; then
-  # source <(kubectl completion zsh)
-# fi
-
 # Add support to the terminal for colours.
 #   See: https://github.com/nathanbuchar/atom-one-dark-terminal
 export CLICOLOR=1
@@ -46,8 +36,46 @@ export EDITOR=vim
 export TERM="xterm-256color"
 alias tmux="tmux -2"
 
-# Current preferred prompt.
-export PS1="\[\033[38;5;10m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;28m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;2m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\n\\$ \[$(tput sgr0)\]"
+if [[ $COLORTERM = gnome-* && $TERM = xterm ]]  && infocmp gnome-256color >/dev/null 2>&1; then export TERM=gnome-256color
+elif infocmp xterm-256color >/dev/null 2>&1; then export TERM=xterm-256color
+fi
+
+# Set some variables for prompt colours.
+set_ps1 () {
+    local darkgray="\e[0;30m"  # Dark Gray
+    local darkgraybold="\e[1;30m" # Bold Dark Gray
+    local red="\e[0;31m" # Red
+    local redbold="\e[1;31m" # Bold Red
+    local green="\e[0;32m" # Green
+    local greenbold="\e[1;32m" # Bold Green
+    local yellow="\e[0;33m" # Yellow
+    local yellowbold="\e[1;33m" # Bold Yellow
+    local blue="\e[0;34m" # Blue
+    local bluebold="\e[1;34m" # Bold Blue
+    local purple="\e[0;35m" # Purple
+    local purplebold="\e[1;35m" # Bold Purple
+    local turquoise="\e[0;36m" # Turquoise
+    local turquoisebold="\e[1;36m" # Bold Turquoise
+    local lightgray="\e[0;37m" # Light Gray
+    local lightgraybold="\e[1;37m" # Bold Light Gray
+    local bg_darkgray="\e[40m" # Dark Gray (Background)
+    local bg_red="\e[41m" # Red (Background)
+    local bg_green="\e[42m" # Green (Background)
+    local bg_yellow="\e[43m" # Yellow (Background)
+    local bg_blue="\e[44m" # Blue (Background)
+    local bg_purple="\e[45m" # Purple (Background)
+    local bg_turquoise="\e[46m" # Turquoise (Background)
+    local bg_lightgray="\e[47m" # Light Gray (Background)
+
+    # Setup the PS1 line if we are using bash.
+    if [ -n "$BASH_VERSION" ]; then
+        # Host, User, Working Directory
+        # export PS1="\[\033[38;5;10m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;28m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;2m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\n\\$ \[$(tput sgr0)\]"
+        # User, Working Directory. A little cleaner than the above.
+        export PS1="\[$(tput sgr0)\]\[\033[38;5;28m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;2m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\n\\$ \[$(tput sgr0)\]"
+    fi
+}
+set_ps1
 
 # Set the language. This is required for some Python tools.
 # Fix 'perl: warning: Setting locale failed.'
@@ -67,6 +95,6 @@ if [ -n "$BASH_VERSION" ]; then
     if [ -f /etc/bash_completion ]; then . /etc/bash_completion; fi
 elif [ -n "$ZSH_VERSION" ]; then
     # Source zsh auto-completions.
-    fpath=(~/.zsh/completion $fpath)
+    fpath=($HOME/.zsh/completion $fpath)
     autoload -Uz compinit && compinit -i
 fi
---
+++ setup.d/kubernetes-autocompletion.sh
@@ -0,0 +1,8 @@
+# Bit of an issue with this - if we are in zsh we need zsh auto-completion, but
+# we're using a bash shebang...
+
+# If kubectl is installed, enable auto completion.
+# if [ -x "$(command -v kubectl)" ]; then
+  # source <(kubectl completion zsh)
+# fi
+
