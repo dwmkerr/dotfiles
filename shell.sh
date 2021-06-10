a1efab5d3085f03e5d707894f4531ed703dcb3bc
Dave Kerr <dwmkerr@gmail.com>
2021-06-10 17:21:11 +0800

    feat: add the 'use_ps1' script to simplify ps1 setup
    Also better setup for the bashrc and zshrc files.

--- shell.sh
+++ shell.sh
@@ -9,6 +9,16 @@
 # an example of why this matters. If we use a shebang in this script, we'll only
 # ever source auto-completions for the shell in the shebang.
 
+# If we are not running interactively do not continue loading this file.
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# Set our preferred editor (both visual and line mode to be safe).
+EDITOR=vi
+VISUAL=vi
+
 # Import everything from the .shell.d folder.
 for file in $HOME/.shell.d/*; do
     [ -e "$file" ] || continue
@@ -24,13 +34,37 @@ if [[ -d $HOME/.shell-private.d ]]; then
     done
 fi
 
+# Set a shell option but don't fail if it doesn't exist!
+safe_set() { shopt -s "$1" >/dev/null 2>&1 || true; }
+
+# Set some options to make working with folders a little easier. Note that we
+# send all output to '/dev/null' as startup files should not write to the
+# terminal and older shells might not have these options.
+safe_set autocd         # Enter a folder name to 'cd' to it.
+safe_set cdspell        # Fix minor spelling issues with 'cd'.
+safe_set dirspell       # Fix minor spelling issues for commands.
+safe_set cdable_vars    # Allow 'cd varname' to switch directory.
+
+# Uncomment the below if you want to be able to 'cd' into directories that are
+# not just relative to the current location. For example, if the below was
+# uncommented we could 'cd my_project' from anywhere if 'my_project' is in
+# the 'repos' folder.
+# CDPATH="~:~/repos"
+
+# Configure the history to make it large and support multi-line commands.
+safe_set histappend                  # Don't overwrite the history file, append.
+safe_set cmdhist                     # Multi-line commands are one entry only.
+PROMPT_COMMAND='history -a'          # Before we prompt, save the history.
+HISTSIZE=10000                       # A large number of commands per session.
+HISTFILESIZE=100000                  # A huge number of commands in the file.
+# HISTCONTROL="ignorespace:ignoredup" # Ignore starting with space or duplicates?
+# export HISTIGNORE="ls:history"     # Any commands we want to not record?
+# HISTTIMEFORMAT='%F %T '            # Do we want a timestamp for commands?
+
 # Add support to the terminal for colours.
 #   See: https://github.com/nathanbuchar/atom-one-dark-terminal
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
-
-# For any programs which need an editor, we use vim.
-export EDITOR=vim
 
 # We're an xterm 256bit colour terminal, just in case anyone asks...
 export TERM="xterm-256color"
@@ -44,9 +78,6 @@ fi
 # Fix 'perl: warning: Setting locale failed.'
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-
-# If we are not running in interactive mode, we're done.
-[[ $- != *i* ]] && return
 
 # We *are* interactive, so if we are not already in tmux, start it.
 [[ -z "$TMUX" ]] && exec tmux
@@ -64,4 +95,5 @@ elif [ -n "$ZSH_VERSION" ]; then
     autoload -Uz compinit && compinit -i
 fi
 
-# Set the PS1 theme.
+# Set my preferred prompt.
+set_ps1 "dwmkerr" || true
