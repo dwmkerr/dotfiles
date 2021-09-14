9ecb95be16256c514b8395f73de3685d1b2c46dc
Dave Kerr <dwmkerr@gmail.com>
2021-09-14 14:22:09 +0800

    chore(aliases): update 'serve' for python 3
    Better would be to write a function that checks the Python version, but
    this can be something for another time.

--- shell.d/aliases.sh
+++ shell.d/aliases.sh
@@ -13,8 +13,9 @@
 # Remove formatting from text in the clipboard.
 alias clipclean="pbpaste | pbcopy"
 
-# Start a web server
-alias serve="python -m SimpleHTTPServer 3000"
+# Start a web server, Python 2 or 3.
+alias serve_python2="python -m SimpleHTTPServer 3000"
+alias serve="python -m http.server 3000"
 
 # When we use `ag`, provide the a shared `.ignore` file.
 alias ag='ag --path-to-ignore ~/.ignore'
