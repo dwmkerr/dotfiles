544d5a65a5c1f97ae88459f41c2d6046f69efe02
Dave Kerr <dwmkerr@gmail.com>
2022-07-27 12:44:34 +0800

    refactor: clean up shell setup

--- setup.d/05-zsh.sh
+++ setup.d/05-zsh.sh
@@ -1,15 +1,3 @@
-# Install the latest bash.
-if ask "$os: install or upgrade bash and bash-completion?" N; then
-    if [[ "$os" == "osx" ]]; then
-        brew install bash bash-completion@2
-        # Make sure the installed zsh path is allowed in the list of shells.
-        echo "$(which bash)" >> sudo tee -a /etc/shells
-    elif [[ "$os" == "ubuntu" ]]; then
-        echo "$os: not yet implemented"
-        exit 1
-    fi
-fi
-
 # Install the latest zsh.
 if ask "$os: install or upgrade zsh?" N; then
     if [[ "$os" == "osx" ]]; then
@@ -45,26 +33,31 @@ if [[ ! "$SHELL" =~ zsh$ ]]; then
 fi
 
 # Check the shell, and make sure that we are sourcing the .shell.sh file.
-if ask "$os: Add .shell.sh to bash/zsh?" Y; then
+if ask "$os: Add .shell.sh to .zshrc?" Y; then
 
     # Create the .shell.sh script symlink as well as shell.d folder symlink.
 	ensure_symlink "$(pwd)/shell.sh" "$HOME/.shell.sh"
 	ensure_symlink "$(pwd)/shell.d" "$HOME/.shell.d"
-    
-    # Source our shell configuration in any local shell config files.
-    config_files=(~/.bashrc ~/.zshrc)
-    for config_file in ${config_files[@]}; do
-        # Skip config files that don't exist.
-        ! [ -r "${config_file}" ] && continue
 
-        # If we don't have the 'source ~/.shell.d' line in our config, add it.
-        source_command="[ -r ~/.shell.sh ] && source ~/.shell.sh"
-        if ! grep -q "${source_command}" "${config_file}"; then
-            if ask "$os: .shell.sh is not sourced in '${config_file}' add it?" Y; then
-                echo "" >> "${config_file}"
-                echo "# Source my personal (github.com/dwmkerr/dotfiles) configuration." >> "${config_file}"
-                echo "${source_command}" >> "${config_file}"
+    # Source our shell configuration in any local shell config files.
+    config_file="${HOME}/.zshrc"
+
+    # Check if we are sourcing the dotfiles shell config.
+    source_command="[ -r ~/.shell.sh ] && source ~/.shell.sh"
+    if ! grep -q "${source_command}" "${config_file}"; then
+
+        # If the config file doesn't exist, offer to create it.
+        if ! [ -r "${config_file}" ]; then
+            if ask "$os: shell config ${config_file} does not exist - create it now?" Y; then
+                touch "${config_file}"
             fi
         fi
-    done
+
+        # Ask the user if they'd like to source the shell config.
+        if ask "$os: .shell.sh is not sourced in '${config_file}' add it?" Y; then
+            echo "" >> "${config_file}"
+            echo "# Source my personal (github.com/dwmkerr/dotfiles) configuration." >> "${config_file}"
+            echo "${source_command}" >> "${config_file}"
+        fi
+    fi
 fi
