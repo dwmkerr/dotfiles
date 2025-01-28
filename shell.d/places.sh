b08521f7f36ed62a542eb7963962180698552906
Dave Kerr <dwmkerr@gmail.com>
2025-01-28 23:28:07 +0000

    fix: simplify places/shortcuts

--- shell.d/places.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-alias gocode='cd ~/repos/github/dwmkerr'
-alias gobox='cd ~/Box\ Sync'
-alias gorepos='cd ~/repos'
-alias goandroidstudio='open -a /Applications/Android\ Studio.app'
--- shell.d/shortcuts.sh
+++ shell.d/shortcuts.sh
@@ -1,1 +1,4 @@
-alias repos="cd ~/repos"
+alias gocode='cd ~/repos/github/dwmkerr'
+alias gobox='cd ~/Box\ Sync'
+alias gorepos='cd ~/repos'
+alias goandroidstudio='open -a /Applications/Android\ Studio.app'
